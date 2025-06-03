3397bb28735326f660fe01e86a9fefff5a1e61af
OleksiiKasian <kasianalex1@gmail.com>
2025-06-03 13:14:28 +0300

    add initial files

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,59 +1,63 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.getElementById('gallery');
-const loader = document.getElementById('loader');
-const loadMoreBtn = document.getElementById('load-more');
-
-let lightbox = new SimpleLightbox('.gallery a');
+let lightbox;
 
 export function createGallery(images) {
+  const gallery = document.getElementById('gallery');
+
   const markup = images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `
-    <li class="photo-card">
-      <a href="${largeImageURL}">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      </a>
-      <div class="info">
-        <p class="info-item"><b>Likes:</b> ${likes}</p>
-        <p class="info-item"><b>Views:</b> ${views}</p>
-        <p class="info-item"><b>Comments:</b> ${comments}</p>
-        <p class="info-item"><b>Downloads:</b> ${downloads}</p>
-      </div>
-    </li>
-  `
-    )
+    .map(image => {
+      return `
+        <li class="gallery-item">
+          <a href="${image.largeImageURL}">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+          </a>
+          <div class="info">
+            <div class="info-item">
+              <b>Likes</b>${image.likes}
+            </div>
+            <div class="info-item">
+              <b>Views</b>${image.views}
+            </div>
+            <div class="info-item">
+              <b>Comments</b>${image.comments}
+            </div>
+            <div class="info-item">
+              <b>Downloads</b>${image.downloads}
+            </div>
+          </div>
+        </li>
+      `;
+    })
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', markup);
-  lightbox.refresh();
+
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a');
+  } else {
+    lightbox.refresh();
+  }
 }
 
 export function clearGallery() {
+  const gallery = document.getElementById('gallery');
   gallery.innerHTML = '';
 }
 
 export function showLoader() {
-  loader.classList.remove('hidden');
+  document.getElementById('loader').classList.remove('hidden');
 }
 
 export function hideLoader() {
-  loader.classList.add('hidden');
+  document.getElementById('loader').classList.add('hidden');
 }
 
 export function showLoadMoreButton() {
-  loadMoreBtn.classList.remove('hidden');
+  document.getElementById('load-more').classList.remove('hidden');
 }
 
 export function hideLoadMoreButton() {
-  loadMoreBtn.classList.add('hidden');
+  document.getElementById('load-more').classList.add('hidden');
 }
